3b37cab60a074ab58d07256ff37c1f7bb771e02f
Katherine Fritsch <katherinefritsch@KatherisMacBook.lan>
2019-11-24 13:57:04 -0800

    Add section 4 and change transition speed

--- README.md
+++ README.md
@@ -11,3 +11,8 @@ The starter project has some HTML and CSS styling to display a static version of
 To get started, open `js/app.js` and start building out the app's functionality
 
 For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+
+## Overview
+
+This is a multi-section landing page. I dynamically add the content to the page using javascript. The navbar is added using javascript. This helped me to understand the virtual DOM.
+
--- js/app.js
+++ js/app.js
@@ -1,34 +1,3 @@
-/**
- *
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
- *
- */
-
-/**
- * Define Global Variables
- *
- */
-
-/**
- * End Global Variables
- * Start Helper Functions
- *
- */
-
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
 
 // build the nav
 document.addEventListener("DOMContentLoaded", init);
@@ -55,22 +24,6 @@ function init() {
             a.classList.remove('active');
             s.classList.remove('your-active-class');
         }
-    },{threshold:0.01})).observe(s);
+    },{threshold:0.5})).observe(s);
   });
 }
-
-// Add class 'active' to section when near top of viewport
-
-// Scroll to anchor ID using scrollTO event
-
-/**
- * End Main Functions
- * Begin Events
- *
- */
-
-// Build menu
-
-// Scroll to section on link click
-
-// Set sections as active
